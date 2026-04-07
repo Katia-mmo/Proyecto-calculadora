019ea49fd1581990deef618db7cf07d059d1269a
Katia-mmo <milanez.katia01@gmail.com>
2026-04-07 20:59:13 +0200

    Remove Persona instances and related functionality
    Removed Persona instances and related code for displaying and modifying personal data.

--- PracticaGetySetIV.java
+++ PracticaGetySetIV.java
@@ -3,7 +3,6 @@
 package com.mycompany.practicagetysetiv;
 
 import java.util.Scanner;
-import persona.Persona;
 import vector.Vector1;
 import vector.Vector2;
 
@@ -23,9 +22,6 @@ public class PracticaGetySetIV {
         int[] vectorC = new int[20];
         
         Numero1 calculo1 = new Numero1();
-        Persona persona1=new Persona();
-        Persona persona2 = new Persona();
-        Persona persona3 = new Persona();
         Vector1 v1 = new Vector1();
         Vector2 v2 = new Vector2();
 
@@ -74,29 +70,5 @@ public class PracticaGetySetIV {
         v2.calcular(vectorA, vectorB, vectorC);
         v2.mostrarVectores(vectorA, vectorB, vectorC);
         
-
-        
-        persona1.setNombre("Joana");
-        persona1.setApellido("Sanchez");
-        persona1.setCalle("Gran Via 2");
-        persona1.setTelefono("986548687");
-        persona1.setAficiones("leer, viajar, correr");
-        persona1.mostrarDatos();
-        
-        persona2.setNombre("Pedro");
-        persona2.setApellido("Martinez");
-        persona2.setCalle("Ramon y Cajal 13");
-        persona2.setTelefono("654987123");
-        persona2.setAficiones("Pintar, dibujar , leer");
-        persona2.mostrarDatos();
-              
-        persona3.setNombre("Lucas");
-        persona3.setApellido("Fernandez");
-        persona3.setCalle("Blasco Ibañez 15");
-        persona3.setTelefono("698574321");
-        persona3.setAficiones("deportes, escribir, comprar");
-        persona3.mostrarDatos();
-        persona3.cambiarDatos();
-        persona3.mostrarDatos();
     }
 }
